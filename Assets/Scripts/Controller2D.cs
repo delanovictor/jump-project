7028e149bb42bd5fa35bf5a63c4b4a892f5750b1
yodabranco <74119629+yodabranco@users.noreply.github.com>
2020-11-29 21:26:20 -0300

    Add files via upload
    Assets

---
+++ Assets/Scripts/Controller2D.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (BoxCollider2D))]
+public class Controller2D : MonoBehaviour {
+
+	const float skinWidth = .015f;
+	public int horizontalRayCount = 4;
+	public int verticalRayCount = 4;
+	public LayerMask collisionMask;
+	float horizontalRaySpacing;
+	float verticalRaySpacing;
+	public CollisionInfo collisions;
+
+	BoxCollider2D bc2d;
+	RaycastOrigins raycastOrigins;
+	
+
+	// Função chamada no script do player, recebe uma velocidade, checa se há colisões e move o objeto.
+	public void Move(Vector2 velocity){
+		UpdateRaycastOrigins ();
+		collisions.Reset();
+		if(velocity.x != 0){
+			HorizontalCollisions(ref velocity);
+		}
+		if(velocity.y != 0){
+			VerticalCollisions(ref velocity);
+		}
+		transform.Translate(velocity);
+	}
+	
+	void Start() {
+		bc2d = GetComponent<BoxCollider2D> ();
+		CalculateRaySpacing ();
+	}
+
+// ----------------------------------------------
+// --------------- RAY CAST ---------------------
+// ----------------------------------------------
+
+	//Calcula a distancia entre os rays, baseando-se na quantidade deles e no tamanho do box collider
+	void CalculateRaySpacing() {
+		Bounds bounds = bc2d.bounds;
+		bounds.Expand (skinWidth * -2);
+
+		horizontalRayCount = Mathf.Clamp (horizontalRayCount, 2, int.MaxValue);
+		verticalRayCount = Mathf.Clamp (verticalRayCount, 2, int.MaxValue);
+
+		horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+		verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+	}
+
+	//Calcula as posições rays importantes
+	void UpdateRaycastOrigins() {
+		Bounds bounds = bc2d.bounds;
+		bounds.Expand (skinWidth * -2);
+
+		raycastOrigins.bottomLeft = new Vector2 (bounds.min.x, bounds.min.y);
+		raycastOrigins.bottomRight = new Vector2 (bounds.max.x, bounds.min.y);
+		raycastOrigins.topLeft = new Vector2 (bounds.min.x, bounds.max.y);
+		raycastOrigins.topRight = new Vector2 (bounds.max.x, bounds.max.y);
+	}
+
+	// Armazena as posições dos rays importantes
+	struct RaycastOrigins {
+		
+		public Vector2 topLeft, topRight;
+		public Vector2 bottomLeft, bottomRight;
+	}
+
+
+// ----------------------------------------------
+	//Desenha os rays verticalmente e horizontalmente, caso eles acertem algo seta as variaveis do contruct collisions
+	void VerticalCollisions(ref Vector2 velocity){
+		float directionY = Mathf.Sign(velocity.y);
+		float rayLenght = Mathf.Abs(velocity.y) + skinWidth;
+		for (int i = 0; i < verticalRayCount; i ++) {
+			Vector2 rayOrigin = (directionY == -1)?raycastOrigins.bottomLeft:raycastOrigins.topLeft;
+			rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+			RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLenght, collisionMask);
+			Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLenght ,Color.red);
+
+
+			if(hit){
+				velocity.y = (hit.distance - skinWidth) * directionY;
+				rayLenght = hit.distance;
+
+				collisions.bellow = (directionY == -1);
+				collisions.above = (directionY == 1);
+			}
+		}
+	}
+
+	void HorizontalCollisions(ref Vector2 velocity){
+		float directionX = Mathf.Sign(velocity.x);
+		float rayLenght = Mathf.Abs(velocity.x) + skinWidth;
+		for (int i = 0; i < horizontalRayCount; i ++) {
+			Vector2 rayOrigin = (directionX == -1)?raycastOrigins.bottomLeft:raycastOrigins.bottomRight;
+			rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+			RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLenght, collisionMask);
+			Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLenght ,Color.red);
+			if(hit){
+				velocity.x = (hit.distance - skinWidth) * directionX;
+				rayLenght = hit.distance;
+
+				collisions.left = directionX == -1;
+				collisions.right = directionX == 1;
+			}
+		}
+	}
+// --------------------------------------------------
+		public struct CollisionInfo{
+		public bool above, bellow, left, right;
+
+		public void Reset(){
+			above = bellow = false;
+			right = left = false;
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour {
+
+    //Esses três parâmetros determinam todos os outros
+    public float jumpHeight = 4;
+    public float timeToJumpPeak = .5f;
+    public float moveSpeed = 7;
+    
+    //Esses caras derivam dos de cima. 
+    float gravity;
+    float jumpVelocity;
+
+    Vector2 velocity;
+
+    bool jumping;
+	Controller2D controller;
+
+
+	void Start() {
+        //Instancia classe controller
+		controller = GetComponent<Controller2D> ();
+        //calculo da gravidade e altura do pulo apartir da altura e duração do pulo
+        gravity = (-2 * jumpHeight)/(timeToJumpPeak * timeToJumpPeak);
+        jumpVelocity = 2 * jumpHeight / timeToJumpPeak;
+	}
+
+    private void Update() {
+        Vector2 input = new Vector2();
+
+        //Se houver colisões em cima ou embaixo do personagem:
+            // - Pega os inputs do player
+            // - Zera sua velocidade em y (será mudado para os diferentes tipos de terreno)
+            // - Se o player está no chão, zera a flag de pulo
+
+        if(controller.collisions.above || controller.collisions.bellow){
+            input = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+            velocity.y = 0;
+            if(controller.collisions.bellow){
+                jumping = false;
+            }
+        }
+        //O jogador não pode se movimentar durante o pulo, porém se ele estiver no ar por outra razão
+        // que não seja o pulo, então pode se movimentar.
+        else if(!jumping){
+            input = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        }
+
+        //Esse oldVelocity é usado para melhor simular a trajetória.
+        Vector2 oldVelocity = velocity;
+
+        //Aplica a gravidade nesse frame
+        velocity.y += gravity * Time.deltaTime;
+
+        //Se space foi apertado AND o player está no chão
+        if(Input.GetKey(KeyCode.Space) && controller.collisions.bellow){
+            //Desenha a trajetória e não se move.
+            DrawPath(input.x);
+        }
+        else if(!jumping){
+            //Caso contrário, se não está pulando:
+            //Calcula velocidade em x e move o personagem
+            velocity.x = input.x * moveSpeed;
+            Vector2 deltaPos = (oldVelocity + velocity) * 0.5f * Time.deltaTime;
+            controller.Move(deltaPos);
+        }
+
+        //Se o botão de espaço for soltado AND estiver em contato com o solo
+        if(Input.GetKeyUp(KeyCode.Space) && controller.collisions.bellow){
+            //Pula para direção do direcional
+            jumping = true;
+            velocity.y = jumpVelocity;
+            velocity.x = input.x * moveSpeed;
+        }
+
+        //Se estiver pulando
+        if(jumping == true){
+            //Movimenta o personagem
+            Vector2 deltaPos = (oldVelocity + velocity) * 0.5f * Time.deltaTime;
+            controller.Move(deltaPos);
+
+            //Se colidir com algo horizontalmente, inverte a velocidade em X e multiplica por um fator
+            //Esse fator será variado conforme o tipo de obstáculo, e será detectado dinâmicamente.
+            if(controller.collisions.right || controller.collisions.left){
+                velocity.x = -velocity.x * 0.5f;
+            }
+        }
+    }
+
+    //Desenha a trajetória, precisa de melhoras.
+    void DrawPath(float direction){
+        Vector2 previousDrawPoint = transform.position;
+        Vector2 initialVelocity = new Vector2();
+        initialVelocity.y += jumpVelocity;
+        initialVelocity.x += direction * moveSpeed;
+
+        float steps = 60;
+        float timeSteps = (timeToJumpPeak*2)/steps;
+
+        for(int i = 1; i<steps; i++){
+            float currentTime = timeSteps * i;
+            Vector2 currentPos = new Vector2(transform.position.x, transform.position.y);
+            Vector2 drawPoint = currentPos + ((initialVelocity * currentTime) + (Vector2.up * gravity * currentTime * currentTime)/2);
+            Debug.DrawLine(previousDrawPoint, drawPoint, Color.green);
+            previousDrawPoint = drawPoint;
+        }
+
+    }
+
+}
